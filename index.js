66c186bf6e2645d3655bbbeea44ce1e2e1ec31a5
Siddhart <Siddhart_7@live.nl>
2022-06-27 16:18:50 +0200

    changes

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ async function getCountries() {
             "https://smspool.net/api/country/retrieve_all"
         )
         .then((response) => { // If the GET request is successful, this block is executed
-            return response; // The response of the API call is passed on to the next block
-        })
-        .then((contests) => { // In this block, we store the response data into a variable 'result'
-            result = contests.data.objects;
+            result = response; // The response of the API call is passed on to the next block
         })
         .catch((err) => {
             console.log(err); // Error handler
